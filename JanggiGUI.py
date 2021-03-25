75c358d3ffd1e62d78b480fa5fce51366610443d
harritra <harritra@oregonstate.edu>
2021-03-24 19:37:10 -0700

    Add display scaling

--- JanggiGUI.py
+++ JanggiGUI.py
@@ -5,47 +5,72 @@ game = JanggiGame()
 board = game.get_board()
 
 pygame.init()
-
 fps = 30
 
-width, height = 900, 1000
+# change board_target_height to adjust the game resolution
+# 1000 is native and sharpest
+board_target_height = 1000
+full_width, full_height = 900, 1000
+scale_factor = board_target_height / full_height
+width, height = round(scale_factor * full_width), round(scale_factor * full_height)
 rows, cols = 10, 9
-text_box_size = 50  # height of screen space below board for displaying text
-screen = pygame.display.set_mode((width, height + text_box_size))  # create window
+
+# height of screen space below board for displaying text
+full_text_box_size = 50
+text_box_size = round(scale_factor * full_text_box_size)
+
+# create the window
+screen = pygame.display.set_mode((width, height + text_box_size))
 pygame.display.set_caption('Janggi')  # window title
 
-board_img = pygame.image.load('images/board.png').convert()  # returns a Surface
-#board_img = pygame.transform.smoothscale(board_img, (450, 500))
-#board_rect = board_img.get_rect()  # get Rect for storing coordinates
-#board_rect.center = width // 2, height // 2  # center the image
+# load the image as a Surface
+board_img = pygame.image.load('images/board.png').convert()
+board_img = pygame.transform.smoothscale(board_img, (width, height))
 
 # define colors and font
-font = pygame.font.Font(None, 44)
+font = pygame.font.Font(None, round(scale_factor * 44))
 red = (200, 0, 0)
 blue = (0, 0, 150)
 green = (0, 255, 0)
 grey = (200, 200, 200)
 black = (0, 0, 0)
 
-# load images
-bca_img = pygame.image.load('images/blue_cannon.png').convert()
-bch_img = pygame.image.load('images/blue_chariot.png').convert()
-bel_img = pygame.image.load('images/blue_elephant.png').convert()
-bge_img = pygame.image.load('images/blue_general.png').convert()
-bgu_img = pygame.image.load('images/blue_guard.png').convert()
-bho_img = pygame.image.load('images/blue_horse.png').convert()
-bso_img = pygame.image.load('images/blue_soldier.png').convert()
-rca_img = pygame.image.load('images/red_cannon.png').convert()
-rch_img = pygame.image.load('images/red_chariot.png').convert()
-rel_img = pygame.image.load('images/red_elephant.png').convert()
-rge_img = pygame.image.load('images/red_general.png').convert()
-rgu_img = pygame.image.load('images/red_guard.png').convert()
-rho_img = pygame.image.load('images/red_horse.png').convert()
-rso_img = pygame.image.load('images/red_soldier.png').convert()
+# load images as Surfaces
+bca_img = pygame.image.load('images/blue_cannon.png').convert_alpha()
+bch_img = pygame.image.load('images/blue_chariot.png').convert_alpha()
+bel_img = pygame.image.load('images/blue_elephant.png').convert_alpha()
+bge_img = pygame.image.load('images/blue_general.png').convert_alpha()
+bgu_img = pygame.image.load('images/blue_guard.png').convert_alpha()
+bho_img = pygame.image.load('images/blue_horse.png').convert_alpha()
+bso_img = pygame.image.load('images/blue_soldier.png').convert_alpha()
+rca_img = pygame.image.load('images/red_cannon.png').convert_alpha()
+rch_img = pygame.image.load('images/red_chariot.png').convert_alpha()
+rel_img = pygame.image.load('images/red_elephant.png').convert_alpha()
+rge_img = pygame.image.load('images/red_general.png').convert_alpha()
+rgu_img = pygame.image.load('images/red_guard.png').convert_alpha()
+rho_img = pygame.image.load('images/red_horse.png').convert_alpha()
+rso_img = pygame.image.load('images/red_soldier.png').convert_alpha()
+
+# scale Surfaces
+bca_img = pygame.transform.smoothscale(bca_img, (round(scale_factor * bca_img.get_width()), round(scale_factor * bca_img.get_height())))
+bch_img = pygame.transform.smoothscale(bch_img, (round(scale_factor * bch_img.get_width()), round(scale_factor * bch_img.get_height())))
+bel_img = pygame.transform.smoothscale(bel_img, (round(scale_factor * bel_img.get_width()), round(scale_factor * bel_img.get_height())))
+bge_img = pygame.transform.smoothscale(bge_img, (round(scale_factor * bge_img.get_width()), round(scale_factor * bge_img.get_height())))
+bgu_img = pygame.transform.smoothscale(bgu_img, (round(scale_factor * bgu_img.get_width()), round(scale_factor * bgu_img.get_height())))
+bho_img = pygame.transform.smoothscale(bho_img, (round(scale_factor * bho_img.get_width()), round(scale_factor * bho_img.get_height())))
+bso_img = pygame.transform.smoothscale(bso_img, (round(scale_factor * bso_img.get_width()), round(scale_factor * bso_img.get_height())))
+rca_img = pygame.transform.smoothscale(rca_img, (round(scale_factor * rca_img.get_width()), round(scale_factor * rca_img.get_height())))
+rch_img = pygame.transform.smoothscale(rch_img, (round(scale_factor * rch_img.get_width()), round(scale_factor * rch_img.get_height())))
+rel_img = pygame.transform.smoothscale(rel_img, (round(scale_factor * rel_img.get_width()), round(scale_factor * rel_img.get_height())))
+rge_img = pygame.transform.smoothscale(rge_img, (round(scale_factor * rge_img.get_width()), round(scale_factor * rge_img.get_height())))
+rgu_img = pygame.transform.smoothscale(rgu_img, (round(scale_factor * rgu_img.get_width()), round(scale_factor * rgu_img.get_height())))
+rho_img = pygame.transform.smoothscale(rho_img, (round(scale_factor * rho_img.get_width()), round(scale_factor * rho_img.get_height())))
+rso_img = pygame.transform.smoothscale(rso_img, (round(scale_factor * rso_img.get_width()), round(scale_factor * rso_img.get_height())))
 
 def draw_game_state():
     """Displays text indicating whose turn it is or who won the game."""
-    text_xy = (10, height + 10)
+    margin = round(scale_factor * 10)
+    text_xy = (margin, height + margin)
     game_state = game.get_game_state()
 
     if game_state == 'UNFINISHED':
@@ -64,7 +89,8 @@ def draw_game_state():
 
 def draw_check():
     """Displays text indicating that a player is in check."""
-    text_xy = (width - 120, height + 10)
+    text_xy = (round(scale_factor * (full_width - 120)),
+               round(scale_factor * (full_height + 10)))
 
     if game.is_in_check(game.get_turn()):
         text_surface = font.render("Check!", True, black)
@@ -76,8 +102,8 @@ def position_to_xy(position):
     where a piece should be drawn."""
     col = ord(position[0]) - ord('a')
     row = int(position[1:]) - 1
-    x = col * width // cols - 10
-    y = row * height // rows - 3
+    x = col * width // cols - round(scale_factor * 10)
+    y = row * height // rows - round(scale_factor * 3)
     return x, y
 
 def xy_to_position(xy):
@@ -98,12 +124,6 @@ def draw_pieces():
     for position, piece_id in board.get_board().items():
         if piece_id != '----':
             screen.blit(id_to_img(piece_id), position_to_xy(position))
-
-# Alternative drawing method using rect for each piece
-#bso_rect = bso_img.get_rect()
-#bso_rect.center = 8 * width / cols + 50, 8 * height / rows + 50
-#def draw_piece():
-#screen.blit(bso_img, bso_rect)
 
 def is_own_piece(piece_id):
     """Takes a piece_id and returns True if it belongs to the player whose turn
@@ -132,7 +152,7 @@ def draw_allowed_destinations(piece_id):
 
     for position in allowed_destinations:
         xy = position_to_xy(position)
-        xy_shifted = (xy[0] + 60, xy[1] + 53)
+        xy_shifted = (xy[0] + round(scale_factor * 60), xy[1] + round(scale_factor * 53))
         pygame.draw.circle(screen, green, xy_shifted, size)
 
 # game loop
@@ -164,7 +184,6 @@ while running:
     draw_game_state()  # display text indicating whose turn or who won
     draw_check()  # display text indicating that a player is in check
 
-    #screen.blit(board_img, board_rect)  # draw the board
     screen.blit(board_img, (0, 0))  # draw the board
     draw_pieces()
 
